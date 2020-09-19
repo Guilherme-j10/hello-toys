4b58fc868f0635c03743df956e5648568e93fd5b
Guilherme-j10 <guilhermeZA.123123@gmail.com>
2020-09-18 21:48:37 -0300

    add some changes

--- app/view/Product.php
+++ app/view/Product.php
@@ -12,6 +12,7 @@
             </div>
             <div class="location_info_product">
                 <div class="header_info_product">
+                    <small class="brand">Sunny</small>
                     <h1>Esfera Card Bakugan Battle Planet Vicerox - Sunny 2070</h1>
                     <div class="row_line">
                         <small>(Cód. <?= $dados['id']; ?>)</small>
@@ -23,7 +24,29 @@
             </div>
         </div>
         <div class="small_side_right">
-        
+            <div class="price_information">
+                <h1>R$ 79,90</h1> 
+                <p>Em até <strong>10x</strong> de <strong>R$ 7.99</strong> sem juros </p>
+                <a href="#"><i class="fas fa-long-arrow-alt-right"></i>Consultar mais formas de pagamento</a>
+            </div>
+            <form action="" method="post">
+                <label>Calcule o prazo de entrega</label>
+                <div class="box_ex">
+                    <i class="fas fa-shipping-fast"></i>
+                    <input type="text" name="cep" required placeholder="Digite o seu CEP">
+                    <button type="submit" name="see">Ver</button>
+                </div>
+                <label>Quantidade</label>
+                <div class="qtd">   
+                    <button>-</button>
+                    <input type="text" name="qtd" readonly value="1" required>
+                    <button>+</button>
+                </div>
+                <small>Items disponiveis: 27</small>
+                <div class="payment_option">
+                    <button type="submit" name="pay"><i class="fas fa-shopping-basket"></i> Comprar</button>
+                </div>
+            </form>
         </div>
     </section>
 
